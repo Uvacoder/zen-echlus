c37f2580249b94e74856067bdf021c3d1c128d43
mzaini30 <muhzaini30@gmail.com>
2021-08-05 10:17:19 +0800

    upload

---
+++ src/pages/admin/_layout.svelte
@@ -0,0 +1,9 @@
+<a href="/admin/menu" class="menu"><svg width="1em" height="1em" viewBox="0 0 16 16"><g fill="currentColor"><path d="M16 5H0V4h16v1zm0 8H0v-1h16v1zm0-4.008H0V8h16v.992z"></path></g></svg></a>
+
+<slot></slot>
+
+<style>
+	.menu {
+		@apply fixed right-3 top-3 cursor-pointer user-select-none
+	}
+</style>
---
+++ src/pages/admin/index.svelte
@@ -0,0 +1,3 @@
+<script>
+	document.title = 'Admin'
+</script>
