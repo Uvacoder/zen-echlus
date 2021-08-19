112603ba08ea2ae5f29e6e1362506bfff7506865
mzaini30 <muhzaini30@gmail.com>
2021-08-20 00:17:43 +0800

    upload

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,8 +13,14 @@
 	.kotak h2 {
 		@apply text-2xl pb-2 pt-1
 	}
+	.melayang, .melayang-kiri {
+		@apply fixed bottom-0 p-3 text-green-500
+	}
 	.melayang {
-		@apply fixed bottom-0 right-0 p-3 text-green-500
+		@apply right-0
+	}
+	.melayang-kiri {
+		@apply left-0 cursor-pointer select-none
 	}
 </style>
 
@@ -69,6 +75,14 @@
 	}
 
 	$: dicari && filterTulisan()
+
+	function keAtas(){
+		browser && window.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'smooth'
+		})
+	}
 </script>
 
 <svelte:head>
@@ -88,6 +102,10 @@
 		<p>{x.cuplikan}....</p>
 	</a>
 {/each}
+
+<div class="melayang-kiri" on:click={keAtas}>
+	<svg width="{ukuranIconTulis * 1.07}em" height="{ukuranIconTulis}em" viewBox="0 0 17 16"><path d="M17.027 8a8 8 0 0 0-8-8a8 8 0 0 0-8 8a8 8 0 0 0 8 8a8 8 0 0 0 8-8zM5.154 10c-.205-.186-.205-.774 0-.96l3.467-4.9a.566.566 0 0 1 .746 0l3.478 4.9c.207.185.207.773 0 .96h-7.69z" fill="currentColor" fillRule="evenodd"></path></svg>
+</div>
 
 <a href="/admin/tulisan-baru" class="melayang">
 	<svg width="{ukuranIconTulis}em" height="{ukuranIconTulis}em" viewBox="0 0 24 24"><g fill="none"><path fillRule="evenodd" clipRule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11s11-4.925 11-11S18.075 1 12 1zm1 15a1 1 0 1 1-2 0v-3H8a1 1 0 1 1 0-2h3V8a1 1 0 1 1 2 0v3h3a1 1 0 1 1 0 2h-3v3z" fill="currentColor"></path></g></svg>
